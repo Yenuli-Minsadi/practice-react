83b7e5d10a4e083c9e08df494f387d992a2648e4
Yenuli Minsadi <samararatney@gmail.com>
2025-12-05 18:17:23 +0530

    update with JS expression returns

--- fed2-frontend/src/Yen.jsx
+++ fed2-frontend/src/Yen.jsx
@@ -1,6 +1,14 @@
 function Yen() {
+
+    const name = "Yen";
+    const age = 22;
+    const city = "Maharagama";
     return (
-       <h1>Yen</h1>
+       <>
+           <h1>My name is {name}</h1>
+           <h2>My age is {age}</h2>
+           <h3>I am from {city}</h3>
+       </>
     );
 }
 
